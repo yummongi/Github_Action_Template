972f9ff8ed53e3b39d9cadf968c820f8ce0f0329
yummongi <101652932+yummongi@users.noreply.github.com>
2023-04-04 11:04:33 +0900

    [add] get_num_squarepy file

---
+++ get_num_square.py
@@ -0,0 +1,13 @@
+import os
+
+# get the input and convert it to int
+num = os.environ.get("INPUT_NUM")
+  if num:
+    try:
+        num = int(num)
+    except Exception:
+        exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(num))
+    else:
+      num = 1
+# to set output, print to shell in following syntax
+printf("::set-output name=num_squared::{num ** 2}")
